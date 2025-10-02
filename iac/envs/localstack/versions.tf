d83cb07885bfcdb548f73a7cac128606233789b6
Mark <mlegkovskis@gmail.com>
2025-10-02 14:18:47 +0100

    add localstack deployments

--- iac/envs/localstack/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.4.0"
-  required_providers {
-    aws        = { source = "hashicorp/aws",        version = "~> 5.50" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.29" }
-    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
-    null       = { source = "hashicorp/null",       version = "~> 3.2" }
-  }
-}
